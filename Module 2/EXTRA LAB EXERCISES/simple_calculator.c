67ae43bd4f448bd4262c43d6c69f23232a49b3be
coder-ravi100 <ravirathod100100@gmail.com>
2025-06-03 15:49:42 +0530

    updates

---
+++ Module 2/EXTRA LAB EXERCISES/simple_calculator.c
@@ -0,0 +1,18 @@
+/*Write a C program that acts as a simple calculator. The program should take two numbers 
+and an operator as input from the user and perform the respective operation (addition, 
+subtraction, multiplication, division, or modulus) using operators. 
+• Challenge: Extend the program to handle invalid operator inputs. 
+*/
+
+#include<stdio.h>
+int main()
+{
+    int num1,num2,answer;
+    printf("\n  Enter a number num1  :");
+    scanf("%d",&num1);
+    printf("\n  Enter a number num2  :");
+    scanf("%d",&num2);
+
+    
+    return 0;
+}
