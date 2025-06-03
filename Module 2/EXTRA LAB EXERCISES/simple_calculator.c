4a72402de1591b2c536d0d9aedb50070cd62ae77
coder-ravi100 <ravirathod100100@gmail.com>
2025-06-03 16:01:58 +0530

    Updates

--- Module 2/EXTRA LAB EXERCISES/simple_calculator.c
+++ Module 2/EXTRA LAB EXERCISES/simple_calculator.c
@@ -7,12 +7,35 @@ subtraction, multiplication, division, or modulus) using operators.
 #include<stdio.h>
 int main()
 {
-    int num1,num2,answer;
-    printf("\n  Enter a number num1  :");
-    scanf("%d",&num1);
-    printf("\n  Enter a number num2  :");
-    scanf("%d",&num2);
 
-    
+	int num1,num2;
+	char operators;
+	printf("\n Enter the Number [1] :");
+	scanf("%d",&num1);
+	printf("\n Enter the Number [2] :");
+	scanf("%d",&num2);
+	printf("\n Enter The Operators [+][-][*][/] :");
+	scanf(" %c",&operators);
+	
+	if(operators == '+')
+	{
+		printf("\n Addition  %d",num1+num2);
+	}
+	else if(operators == '-')
+	{
+		printf("\n Subtraction %d",num1-num2);
+	}
+	else if(operators == '*')
+	{
+		printf("\n Multiplication %d",num1*num2);
+	}
+	else if(operators == '/')
+	{
+		printf("\n Division %.2f",(float)num1/(float)num2);
+	}
+	else
+	{
+		printf("\n Invalid operator inputs");
+	}
     return 0;
 }
