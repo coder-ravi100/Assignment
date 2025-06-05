6a918bbdbcdb7ad86463d1ee18f83a61cdf34b99
coder-ravi100 <ravirathod100100@gmail.com>
2025-06-05 18:36:45 +0530

    updates

---
+++ Module 2/EXTRA LAB EXERCISES/Prime_no.c
@@ -0,0 +1,11 @@
+/*3. Loops */
+/*LAB EXERCISE 1: Prime Number Check 
+ 
+• Write a C program that checks whether a given number is a prime number or not using a for 
+loop. 
+• Challenge: Modify the program to print all prime numbers between 1 and a given number. */
+#include<stdio.h>
+int main()
+{
+    return 0;
+}
