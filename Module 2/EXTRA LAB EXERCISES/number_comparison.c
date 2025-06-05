21b8f7cc39965314d4ff4289eb8b585e0b28c9b2
coder-ravi100 <ravirathod100100@gmail.com>
2025-06-05 18:10:13 +0530

    updates

--- Module 2/EXTRA LAB EXERCISES/number_comparison.c
+++ Module 2/EXTRA LAB EXERCISES/number_comparison.c
@@ -7,31 +7,47 @@ o The smallest number.
 int main()
 {
     int num1,num2,num3;
+    int ch;
     printf("\n Enter the Num 1 :");
     scanf("%d",&num1);
     printf("\n Enter the Num 2 :");
     scanf("%d",&num2);
     printf("\n Enter the Num 3 :");
     scanf("%d",&num3);
-
-    
-    if (num1 > num2)
-        {
-            printf("\n Largest Number is :%d",num1);
-        }
-        else if (num1 > num3)
-        {
-            printf("\n Largest Number is :%d",num3);
-        } 
-        if (num2 > num3)
-        {
-
-           printf("\n Largest Number is :%d",num2);
-        }
-        else
-        {
-          printf("\n Largest Number is :%d",num3);  
-        }
-               
+    printf("\n 1. Press For Largest Number... ");
+    printf("\n 2. Press For Smallest Number... ");
+    printf("\n Enter The Choice");
+    scanf("%d",&ch);
+    switch (ch)
+    {
+    case 1:
+    if (num1 > num2 && num1 > num3)
+    {
+        printf("\n Largest Number :%d",num1);
+    }
+    else if (num2 >= num1 && num2 >= num3)
+    {
+       printf("\n Largest Number :%d",num2);
+    }
+    else
+    {
+         printf("\n Largest Number :%d",num3);
+    }
+    break;
+    case 2:
+    if (num1 < num3  && num1 < num3)
+    {
+         printf("\n Smallest Number :%d",num1);
+    }
+    else if (num2 <= num1  && num2 <= num3)
+    {
+        printf("\n Smallest Number :%d",num2);
+    }
+    else
+    {
+        printf("\n Smallest Number :%d",num3); 
+    }
+    break;
+    }
     return 0;
 }
