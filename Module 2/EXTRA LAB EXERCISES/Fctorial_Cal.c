4429ac84bd03fe01c86cdf921e921b4f2267ab98
coder-ravi100 <ravirathod100100@gmail.com>
2025-06-16 16:20:22 +0530

    update

--- Module 2/EXTRA LAB EXERCISES/Fctorial_Cal.c
+++ Module 2/EXTRA LAB EXERCISES/Fctorial_Cal.c
@@ -2,3 +2,23 @@
 • Write a C program that calculates the factorial of a given number using a function. 
 • Challenge: Implement both an iterative and a recursive version of the factorial function and 
 compare their performance for large numbers.*/
+#include<stdio.h>
+int fact(int num)
+{
+
+    int fact=1,i;
+    for ( i = 1; i <= num; i++)
+    {
+        fact = fact * i;
+    }
+    return fact;
+}
+int main()
+{
+    int number,result;
+    printf("\n Enter Value :");
+    scanf("%d",&number);
+    result = fact(number);
+    printf("\n Factorial:%d",result);
+    return 0;
+}
--- Module 2/EXTRA LAB EXERCISES/Fibonacci_Function.c
+++ Module 2/EXTRA LAB EXERCISES/Fibonacci_Function.c
@@ -5,3 +5,21 @@ recursive
 function. 
 • Challenge: Modify the program to calculate the N th Fibonacci number using both iterative 
 and recursive methods. Compare their efficiency. */
+#include<stdio.h>
+int main()
+{
+    int term, first = 0, second = 1,i,next;
+    printf("\n Enter Term:");
+    scanf("%d",&term);
+    for ( i = 1; i < term-2; i++)
+    {
+        next = first + second;
+        second = first;
+        first = next;
+    }
+    
+    printf("\n Fibonacci Number :%d",next);
+    
+    
+       return 0;
+}
